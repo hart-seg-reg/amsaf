5cee3fb948e5c656aacf228089894a0990c148c4
Ian McDonald <ian.c.mcdonald5@gmail.com>
2018-03-07 10:47:45 -0800

    changed casting from signed 64 bit int to unsigned 16 bit

--- amsaf/experiment_ultrasound.py
+++ amsaf/experiment_ultrasound.py
@@ -4,13 +4,13 @@ import amsaf
 
 def run_amsaf():
     unsegmented_image = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial10_30_w1_volume_TRANS.nii"),
-                                  sitk.sitkInt64)
+                                  sitk.sitkUInt16)
     ground_truth = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial10_30_w1_seg2_TRANS.nii"),
-                             sitk.sitkInt64)
+                             sitk.sitkUInt16)
     segmented_image = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial12_30_w3_volume_TRANS.nii"),
-                                sitk.sitkInt64)
+                                sitk.sitkUInt16)
     segmentation = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial12_30_w3_seg_ak2_TRANS.nii"),
-                             sitk.sitkInt64)
+                             sitk.sitkUInt16)
 
     amsaf_results = amsaf.amsaf_eval(unsegmented_image, ground_truth, segmented_image, segmentation, verbose=True)
     amsaf.write_top_k(10, amsaf_results, '/home/ian/amsaf_results')
