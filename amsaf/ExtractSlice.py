30d6e1c435daf62763208c63d150918e2f38d47d
thomashli <thomasli537@gmail.com>
2018-04-03 10:20:27 -0700

    Added some documentation

--- amsaf/ExtractSlice.py
+++ amsaf/ExtractSlice.py
@@ -4,8 +4,29 @@ import os
 
 """
 How to Use:
-example: python ExtractSlice.py /data/trial11_30_w2_seg_TRANS.nii 9 /data/try.nii 0
-/data/trial11_30_w2_seg_TRANS.nii : input segmentation path
+
+python ExtractSlice.py input_file number_of_slices output_file axis_of_slicing
+
+arguments are as follows:
+input_file: File that you want to obtain slices from
+number_of_slices: Partitions input_file and makes equidistant slices along axis
+	IMPORTANT NOTE:
+		the number of slices partitions the whole image, not just the segmented part (a proportion of the slices may return empty vectors)
+output_file: Name of output file
+axis_of_slicing: Determines which plane slices align with (xy,yz,xz), Takes inputs 0,1,2 (else)
+
+	Given an input file with dimensions [400,300,200]
+	0 returns slices along x axis (slices of the form [x,300,200])
+	1 returns slices along y axis (slices of the form [400,y,200])
+	2 returns slices along z axis (slices of the form [400,300,z])
+
+
+
+
+
+example: 
+python ExtractSlice.py /data/trial11_30_w2_seg_TRANS.nii 9 /data/try.nii 0 /data/trial11_30_w2_testseg_TRANS.nii 
+: input segmentation path
 9 : The number of slices extract from the input segmentation
 /data/try : the output slices 
 0 : in which dimension the slices will be extracted, e.g. if the input image size is [400, 300, 200]
@@ -14,8 +35,15 @@ example: python ExtractSlice.py /data/trial11_30_w2_seg_TRANS.nii 9 /data/try.ni
     if this parameter is 2, each slices will be [400, 300, 1]
 """
 
+"""
+How to Use Pt2:
+example python ExtractSlice.py 
+
+
+
+"""
 if len ( sys.argv ) != 5:
-    print("Usage: %s inputImage sliceNumber outputImage" % (sys.argv[0]))
+    print("Usage: %s inputImage sliceNumber outputImage dimension" % (sys.argv[0]))
     sys.exit(1)
 
 sliceNum = int(sys.argv[2])
@@ -27,18 +55,19 @@ size = list(inputImage.GetSize())
 output = sitk.Image(size[0], size[1], size[2],  sitk.sitkFloat32)
 
 if(dim == 0):
-    length = size[0] / sliceNum
-    slices = [i for i in range(length / 2, size[0], length)]
+    length = int(size[0] / sliceNum)
+    slices = [i for i in range(int(length / 2), size[0], length)]
     range1 = size[1]
     range2 = size[2]
 elif(dim == 1):
-    length = size[1] / sliceNum
-    slices = [i for i in range(length / 2, size[1], length)]
+    length = int(size[1] / sliceNum)
+
+    slices = [i for i in range(int(length / 2), size[1], length)]
     range1 = size[0]
     range2 = size[2]
 else:
-    length = size[2] / sliceNum
-    slices = [i for i in range(length / 2, size[2], length)]
+    length = int(size[2] / sliceNum)
+    slices = [i for i in range(int(length / 2), size[2], length)]
     range1 = size[0]
     range2 = size[1]
 
@@ -55,4 +84,4 @@ for i in range(range1):
                 temp = inputImage.GetPixel(i, j, index)
                 output.SetPixel(i, j, index, temp)
 
-sitk.WriteImage(output, str(sys.argv[3]) )
+sitk.WriteImage(output, str(sys.argv[3]))
