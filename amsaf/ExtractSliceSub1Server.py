9d5b775d5470105df424322fc7e37946610b43d3
Thomas <thomasli537@gmail.com>
2018-04-08 00:47:07 +0000

    finished extracting slices for ultrasound_data

--- amsaf/ExtractSliceSub1Server.py
+++ amsaf/ExtractSliceSub1Server.py
@@ -10,11 +10,11 @@ ExtractSlices(input_image, number_of_slices, axis):
 
 def main():
 	#Get folders
-	path30deg = os.listdir("/srv/ultrasound_data/30deg/")
+	path30deg = "/srv/ultrasound_data/30deg/"
 
-	path60deg = os.listdir("/srv/ultrasound_data/60deg/")
+	path60deg = "/srv/ultrasound_data/60deg/"
 
-	path90deg = os.listdir("/srv/ultrasound_data/90deg/")
+	path90deg = "/srv/ultrasound_data/90deg/"
 
 	result_directory_30deg_path = "/home/thomas/amsaf_results/30degslices/"
 
@@ -22,20 +22,21 @@ def main():
 
 	result_directory_90deg_path = "/home/thomas/amsaf_results/90degslices/"
 
-	for segmentation in path30deg:
+	for segmentation in os.listdir(path30deg):
 		if segmentation.find("seg") >= 0:
 			newfile = segmentation[:-4] + "_slices.nii"
-			sitk.WriteImage(ExtractSlices(segmentation, 10, 0),os.path.join(result_directory_30deg_path,newfile))
+			test = os.path.join(path30deg,segmentation)
+			sitk.WriteImage(ExtractSlices(test, 10, 0),os.path.join(result_directory_30deg_path,newfile))
 	
-	for segmentation in path60deg:
+	for segmentation in os.listdir(path60deg):
 		if segmentation.find("seg") >= 0:
 			newfile = segmentation[:-4] + "_slices.nii"
-			sitk.WriteImage(ExtractSlices(segmentation, 10, 0),os.path.join(result_directory_60deg_path,newfile))
+			sitk.WriteImage(ExtractSlices(os.path.join(path60deg,segmentation), 10, 0),os.path.join(result_directory_60deg_path,newfile))
 	
-	for segmentation in path90deg:
+	for segmentation in os.listdir(path90deg):
 		if segmentation.find("seg") >= 0:
 			newfile = segmentation[:-4] + "_slices.nii"
-			sitk.WriteImage(ExtractSlices(segmentation, 10, 0),os.path.join(result_directory_90deg_path,newfile))
+			sitk.WriteImage(ExtractSlices(os.path.join(path90deg,segmentation), 10, 0),os.path.join(result_directory_90deg_path,newfile))
 
 if __name__ == "__main__":
 	main()
