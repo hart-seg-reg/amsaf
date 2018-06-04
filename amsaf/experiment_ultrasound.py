9864c8d3b8a088d7ea88777e061dd24f449cebc6
Chris <chris@calvisitor-10-105-139-218.calvisitor.1918.berkeley.edu>
2018-06-04 13:18:35 -0700

    rewrote paths for experiment ultrasound

--- amsaf/experiment_ultrasound.py
+++ amsaf/experiment_ultrasound.py
@@ -3,17 +3,17 @@ import amsaf
 
 
 def run_amsaf():
-    unsegmented_image = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial10_30_w1_volume_TRANS.nii"),
+    unsegmented_image = sitk.Cast(amsaf.read_image("~/aws_server/srv/ultrasound_data/30deg/trial10_30_w1_volume_TRANS.nii"),
                                   sitk.sitkUInt16)
-    ground_truth = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial10_30_w1_seg2_TRANS.nii"),
+    ground_truth = sitk.Cast(amsaf.read_image("~/aws_server/srv/ultrasound_data/30deg/trial10_30_w1_seg2_TRANS.nii"),
                              sitk.sitkUInt16)
-    segmented_image = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial12_30_w3_volume_TRANS.nii"),
+    segmented_image = sitk.Cast(amsaf.read_image("~/aws_server/srv/ultrasound_data/30deg/trial12_30_w3_volume_TRANS.nii"),
                                 sitk.sitkUInt16)
-    segmentation = sitk.Cast(amsaf.read_image("/srv/ultrasound_data/30deg/trial12_30_w3_seg_ak2_TRANS.nii"),
+    segmentation = sitk.Cast(amsaf.read_image("~/aws_server/srv/ultrasound_data/30deg/trial12_30_w3_seg_ak2_TRANS.nii"),
                              sitk.sitkUInt16)
 
-    amsaf_results = amsaf.amsaf_eval(unsegmented_image, ground_truth, segmented_image, segmentation, verbose=True)
-    amsaf.write_top_k(10, amsaf_results, '/home/ian/amsaf_results')
+    amsaf_results = amsaf.amsaf_eval(unsegmented_image, ground_truth, segmented_image, segmentation, verbose=True, memoize=False)
+    amsaf.write_top_k(10, amsaf_results, '~/aws_server/home/chris/amsaf_results')
 
 
 if __name__ == '__main__':
