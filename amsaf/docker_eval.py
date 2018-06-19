7c4b15300b8d8ee0f53d6f6009b95e308f4e1807
Chris <chris@calvisitor-10-105-139-197.calvisitor.1918.berkeley.edu>
2018-06-19 14:01:56 -0700

    Set up docker code to run

---
+++ amsaf/docker_eval.py
@@ -0,0 +1,112 @@
+import SimpleITK as sitk
+import amsaf
+import sys
+
+def run_amsaf():
+    directory = "/srv/ultrasound_data/30deg/"
+    unsegmented_image = sitk.Cast(amsaf.read_image(directory + "trial10_30_w1_volume_TRANS.nii"),
+                                  sitk.sitkUInt16)
+    ground_truth = sitk.Cast(amsaf.read_image(directory + "trial10_30_w1_seg2_TRANS.nii"),
+                             sitk.sitkUInt16)
+    segmented_image = sitk.Cast(amsaf.read_image(directory + "trial12_30_w3_volume_TRANS.nii"),
+                                sitk.sitkUInt16)
+    segmentation = sitk.Cast(amsaf.read_image(directory + "trial12_30_w3_seg_ak2_TRANS.nii"),
+                             sitk.sitkUInt16)
+
+    amsaf_results = amsaf.amsaf_eval(unsegmented_image, ground_truth, segmented_image, segmentation, get_param_maps(), verbose=True, memoize=False)
+    amsaf.write_top_k(10, amsaf_results, '../../../../home/chris/amsaf_results')
+
+
+def get_param_maps():
+  rigid = {
+    "AutomaticParameterEstimation": ['true'],
+    "AutomaticTransformInitialization": ['true'],
+    "BSplineInterpolationOrder": ['3.000000'],
+    "CheckNumberOfSamples": ['true'],
+    "DefaultPixelValue": ['0.000000'],
+    "FinalBSplineInterpolationOrder": ['3.000000'],
+    "FixedImagePyramid": ['FixedSmoothingImagePyramid'],
+    "ImageSampler": ['RandomCoordinate'],
+    "Interpolator": ['BSplineInterpolator'],
+    "MaximumNumberOfIterations": ['1024.000000'],
+    "MaximumNumberOfSamplingAttempts": ['8.000000'],
+    "Metric": ['AdvancedMattesMutualInformation'],
+    "MovingImagePyramid": ['MovingSmoothingImagePyramid'],
+    "NewSamplesEveryIteration": ['true'],
+    "NumberOfHistogramBins": ['64.000000'],
+    "NumberOfResolutions": ['3.000000'],
+    "NumberOfSamplesForExactGradient": ['4096.000000'],
+    "NumberOfSpatialSamples": ['2000.000000'],
+    "Optimizer": ['AdaptiveStochasticGradientDescent'],
+    "Registration": ['MultiResolutionRegistration'],
+    "ResampleInterpolator": ['FinalBSplineInterpolator'],
+    "Resampler": ['DefaultResampler'],
+    "ResultImageFormat": ['nii'],
+    "Transform": ['EulerTransform'],
+    "WriteIterationInfo": ['false'],
+    "WriteResultImage": ['true'],
+  }
+  affine = {
+    "AutomaticParameterEstimation": ['true'],
+    "CheckNumberOfSamples": ['true'],
+    "DefaultPixelValue": ['0.000000'],
+    "FinalBSplineInterpolationOrder": ['3.000000'],
+    "FixedImagePyramid":
+        ['FixedSmoothingImagePyramid'],
+    "ImageSampler": ['RandomCoordinate'],
+    "Interpolator": ['BSplineInterpolator'],#Linear Interpolator
+    "MaximumNumberOfIterations": ['1024.000000'],#256
+    "MaximumNumberOfSamplingAttempts": ['8.000000'],
+    "Metric": ['AdvancedMattesMutualInformation'],
+    "MovingImagePyramid": ['MovingSmoothingImagePyramid'],
+    "NewSamplesEveryIteration": ['true'],
+    "NumberOfHistogramBins": ['32.000000'],#nonexistant
+    "NumberOfResolutions": ['4.000000'],
+    "NumberOfSamplesForExactGradient": ['4096.000000'],
+    "NumberOfSpatialSamples": ['2048.000000'],
+    "Optimizer": ['AdaptiveStochasticGradientDescent'],
+    "Registration": ['MultiResolutionRegistration'],
+    "ResampleInterpolator": ['FinalBSplineInterpolator'],
+    "Resampler": ['DefaultResampler'],
+    "ResultImageFormat": ['nii'],
+    "Transform": ['AffineTransform'],
+    "WriteIterationInfo": ['false'],
+    "WriteResultImage": ['true'],
+  }
+  bspline = {
+    'AutomaticParameterEstimation': ["true"],
+    'CheckNumberOfSamples': ["true"],
+    'DefaultPixelValue': ['0.000000'],
+    'FinalBSplineInterpolationOrder': ['3.000000'],
+    'FinalGridSpacingInPhysicalUnits': ['4.000000'],
+    'FixedImagePyramid': ['FixedSmoothingImagePyramid'],
+    'GridSpaceSchedule': ['2.803220 1.988100 1.410000 1.000000']
+    'ImageSampler': ['RandomCoordinate'],
+    'Interpolator': ['LinearInterpolator'],
+    'MaximumNumberOfIterations': ['1024.000000'],
+    'MaximumNumberOfSamplingAttempts': ['8.000000'],
+    'Metric': ['AdvancedMattesMutualInformation'],
+    'Metric0Weight': ['0'],
+    'Metric1Weight': ['1.000000'],
+    'MovingImagePyramid': ["MovingSmoothingImagePyramid"],
+    'NewSamplesEveryIteration': ['true'],
+    'NumberOfHistogramBins': ['32.000000'],#nonexistant
+    'NumberOfResolutions': ['4.000000'],
+    'NumberOfSamplesForExactGradient': ['4096.000000'],
+    'NumberOfSpatialSamples': ['2048.000000'],
+    'Optimizer': ['AdaptiveStochasticGradientDescent'],
+    'Registration': ['MultiMetricMultiResolutionRegistration'],
+    'ResampleInterpolator': ['FinalBSplineInterpolator'],
+    'Resampler': ['DefaultResampler'],
+    'ResultImageFormat': ['nii'],
+    'Transform': ['BSplineTransform'],
+    'WriteIterationInfo': ['false'],
+    'WriteResultImage': ['true']
+  }
+
+  return [rigid, affine, bspline]
+
+
+
+if __name__ == '__main__':
+  run_amsaf()
--- amsaf/experiment_ultrasound.py
+++ amsaf/experiment_ultrasound.py
@@ -1,38 +1,22 @@
 import SimpleITK as sitk
 import amsaf
-import cropping
 import sys
 
 def run_amsaf():
-    unsegmented_image = sitk.Cast(amsaf.read_image("../../../../srv/ultrasound_data/30deg/trial10_30_w1_volume_TRANS.nii"),
+    directory = "/srv/ultrasound_data/30deg/"
+    unsegmented_image = sitk.Cast(amsaf.read_image(directory + "trial10_30_w1_volume_TRANS.nii"),
                                   sitk.sitkUInt16)
-    ground_truth = sitk.Cast(amsaf.read_image("../../../../srv/ultrasound_data/30deg/trial10_30_w1_seg2_TRANS.nii"),
+    ground_truth = sitk.Cast(amsaf.read_image(directory + "trial10_30_w1_seg2_TRANS.nii"),
                              sitk.sitkUInt16)
-    segmented_image = sitk.Cast(amsaf.read_image("../../../../srv/ultrasound_data/30deg/trial12_30_w3_volume_TRANS.nii"),
+    segmented_image = sitk.Cast(amsaf.read_image(directory + "trial12_30_w3_volume_TRANS.nii"),
                                 sitk.sitkUInt16)
-    segmentation = sitk.Cast(amsaf.read_image("../../../../srv/ultrasound_data/30deg/trial12_30_w3_seg_ak2_TRANS.nii"),
+    segmentation = sitk.Cast(amsaf.read_image(directory + "trial12_30_w3_seg_ak2_TRANS.nii"),
                              sitk.sitkUInt16)
 
     amsaf_results = amsaf.amsaf_eval(unsegmented_image, ground_truth, segmented_image, segmentation, verbose=True, memoize=False)
     amsaf.write_top_k(10, amsaf_results, '../../../../home/chris/amsaf_results')
 
 
-def run_split(parameters):
-
-
-
-
-
-
-def run_crop(parameters):
-
-
-
 
 if __name__ == '__main__':
-    if sys.argv[0] == "split":
-      run_split(sys.argv)
-    elif sys.argv[0] == "crop":
-      run_crop(sys.argv)
-    else:
-      run_amsaf()
+  run_amsaf()
