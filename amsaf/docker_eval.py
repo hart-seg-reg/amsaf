dd22c66841a923bd93a4078e553f315d6ad3ba21
Chris <chris@calvisitor-10-105-139-197.calvisitor.1918.berkeley.edu>
2018-06-19 14:06:53 -0700

    Fixed small bug

--- amsaf/docker_eval.py
+++ amsaf/docker_eval.py
@@ -80,7 +80,7 @@ def get_param_maps():
     'FinalBSplineInterpolationOrder': ['3.000000'],
     'FinalGridSpacingInPhysicalUnits': ['4.000000'],
     'FixedImagePyramid': ['FixedSmoothingImagePyramid'],
-    'GridSpaceSchedule': ['2.803220 1.988100 1.410000 1.000000']
+    'GridSpaceSchedule': ['2.803220 1.988100 1.410000 1.000000'],
     'ImageSampler': ['RandomCoordinate'],
     'Interpolator': ['LinearInterpolator'],
     'MaximumNumberOfIterations': ['1024.000000'],
