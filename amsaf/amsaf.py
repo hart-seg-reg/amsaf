60c3076e404c411129e8b4f16b3086b5931643aa
Chris Mitchell <chrism0318@gmail.com>
2018-08-03 12:10:51 -0700

    Implemented memoization

--- amsaf/amsaf.py
+++ amsaf/amsaf.py
@@ -12,6 +12,11 @@ ranking the results of parameter map instances in a caller-defined search space.
 """
 
 import os
+import sys
+import glob
+
+from joblib import Parallel, delayed
+import multiprocessing as mp
 
 import SimpleITK as sitk
 from sklearn.model_selection import ParameterGrid
@@ -27,7 +32,8 @@ def amsaf_eval(unsegmented_image,
                segmented_image,
                segmentation,
                parameter_priors=None,
-               verbose=False):
+               verbose=False,
+               memoize=False):
     """Main AMSAF functionality
 
     Generate and score new segmentations and corresponding Elastix parameter
@@ -48,17 +54,18 @@ def amsaf_eval(unsegmented_image,
                              consideration.
     :param verbose: Optional boolean flag to toggle verbose stdout printing from
                     Elastix.
+    :param memoize: Optional boolean flag to toggle memoized optimization. Warning: experimental
     :type unsegmented_image: SimpleITK.Image
     :type ground_truth: SimpleITK.Image
     :type segmented_image: SimpleITK.Image
     :type segmentation: SimpleITK.Image
     :type parameter_priors: dict
     :type verbose: bool
+    :type memoize: bool
     :returns: A lazy stream of result
               (parameter map vector, result segmentation, segmentation score) lists.
     :rtype: generator
     """
-
     def eval_pm(parameter_map):
         seg = segment(
             unsegmented_image,
@@ -66,7 +73,10 @@ def amsaf_eval(unsegmented_image,
             segmentation,
             parameter_map,
             verbose=verbose)
-        score = _sim_score(seg, ground_truth)
+        if ground_truth is not None:
+            score = _sim_score(seg, ground_truth)
+        else:
+            score = 0
         return [parameter_map, seg, score]
 
     def param_combinations(option_dict, transform_type):
@@ -76,10 +86,30 @@ def amsaf_eval(unsegmented_image,
     if not parameter_priors:
         parameter_priors = _get_default_vector()
 
-    for rpm in param_combinations(parameter_priors[0], 'rigid'):
-        for apm in param_combinations(parameter_priors[1], 'affine'):
-            for bpm in param_combinations(parameter_priors[2], 'bspline'):
-                yield eval_pm([rpm, apm, bpm])
+    if memoize:
+        for rpm in param_combinations(parameter_priors[0], 'rigid'):
+            rigid_image, rigid_pm = register_indv(unsegmented_image, segmented_image, 'rigid', rpm, verbose=verbose)
+            for apm in param_combinations(parameter_priors[1], 'affine'):
+                affine_image, affine_pm = register_indv(rigid_image, segmented_image, 'affine', apm, auto_init=False, verbose=verbose)
+                for bpm in param_combinations(parameter_priors[2], 'bspline'):
+                    bspline_image, bspline_pm = register_indv(affine_image, segmented_image,'bspline', bpm, verbose=verbose)
+                    transform_parameter_maps = [rigid_pm, affine_pm, bspline_pm]
+                    transformed_seg = transform(segmentation, [_nn_assoc_indv(pm[0]) for pm in transform_parameter_maps], verbose=verbose)
+                    if ground_truth is not None:
+                        score = _sim_score(transformed_seg, ground_truth)
+                    else:
+                        score = 0
+                    yield [ transform_parameter_maps , transformed_seg, score]
+
+    else:
+        for rpm in param_combinations(parameter_priors[0], 'rigid'):
+            for apm in param_combinations(parameter_priors[1], 'affine'):
+                for bpm in param_combinations(parameter_priors[2], 'bspline'):
+                    yield eval_pm([rpm, apm, bpm])
+
+
+
+
 
 
 def write_top_k(k, amsaf_results, path):
@@ -133,7 +163,7 @@ def register(fixed_image,
     if not parameter_maps:
         parameter_maps = [
             sitk.GetDefaultParameterMap(t)
-            for t in ['translation', 'affine', 'bspline']
+            for t in ['rigid', 'affine', 'bspline']
         ]
     if auto_init:
         parameter_maps = _auto_init_assoc(parameter_maps)
@@ -146,6 +176,49 @@ def register(fixed_image,
     transform_parameter_maps = registration_filter.GetTransformParameterMap()
 
     return result_image, transform_parameter_maps
+
+def register_indv(fixed_image,
+             moving_image,
+             transform_type,
+             parameter_map=None,
+             auto_init=True,
+             verbose=False):
+    """Register images using Elastix. Used to perform transforms individually
+        Namely used for memoization to avoid redundant computation
+
+    :param transform_type: Type of tranform to be performed
+    :type transform_type: String
+    :param parameter_map: Optional parameter map to be used for
+                           registration. If none is provided, a default map based on transform type is used.
+    :param auto_init: Auto-initialize images. This helps with flexibility when
+                      using images with little overlap.
+    :param verbose: Flag to toggle stdout printing from Elastix
+    :type fixed_image: SimpleITK.Image
+    :type moving_image: SimpleITK.Image
+    :type parameter_map: SimpleITK.ParameterMap
+    :type auto_init: bool
+    :type verbose: bool
+    :returns: Tuple of (result_image, transform_parameter_maps)
+    :rtype: (SimpleITK.Image, [SimpleITK.ParameterMap])
+    """
+    registration_filter = sitk.ElastixImageFilter()
+    if not verbose:
+        registration_filter.LogToConsoleOff()
+    registration_filter.SetFixedImage(fixed_image)
+    registration_filter.SetMovingImage(moving_image)
+
+    if not parameter_map:
+        parameter_map = sitk.GetDefaultParameterMap(transform_type)
+
+    if auto_init:
+        parameter_map = _auto_init_assoc_indv(parameter_map)
+    registration_filter.SetParameterMap(parameter_map)
+    registration_filter.Execute()
+    result_image = registration_filter.GetResultImage()
+    transform_parameter_map = registration_filter.GetTransformParameterMap()
+
+    return result_image, transform_parameter_map
+
 
 
 def segment(unsegmented_image,
@@ -178,6 +251,7 @@ def segment(unsegmented_image,
         segmentation, _nn_assoc(transform_parameter_maps), verbose=verbose)
 
 
+
 def transform(image, parameter_maps, verbose=False):
     """Transform an image according to some vector of parameter maps
 
@@ -195,20 +269,23 @@ def transform(image, parameter_maps, verbose=False):
     transform_filter.SetTransformParameterMap(parameter_maps)
     transform_filter.SetMovingImage(image)
     transform_filter.Execute()
-    result_image = transform_filter.GetResultImage()
+    image = transform_filter.GetResultImage()
+    return image
 
-    return result_image
 
-
-def read_image(path):
+def read_image(path, ultrasound_slice=False):
     """Load image from filepath as SimpleITK.Image
 
     :param path: Path to .nii file containing image.
+    :param ultrasound_slice: Optional. If True, image will be cast as sitkUInt16 for ultrasound images.
     :type path: str
     :returns: Image object from path
     :rtype: SimpleITK.Image
     """
-    return sitk.ReadImage(path)
+    image = sitk.ReadImage(path)
+    if ultrasound_slice:
+        image = sitk.Cast(image, sitk.sitkUInt16)
+    return image
 
 
 def write_image(image, path):
@@ -251,25 +328,118 @@ def write_result(amsaf_result, path):
 def top_k(k, amsaf_results):
     """Get top k results of amsaf_eval
 
-    :param k: Number of results to return
+    :param k: Number of results to return. If k == 0, returns all results
     :param amsaf_results: Results in the format of amsaf_eval return value
     :type k: int
     :type amsaf_result: [[SimpleITK.ParameterMap, SimpleITK.Image, float]]
     :returns: Top k result groups ordered by score
     :rtype: [[SimpleITK.ParameterMap, SimpleITK.Image, float]]
     """
+    if k == 0:
+        sorted(amsaf_results, key=lambda x: x[-1], reverse=True)
     return sorted(amsaf_results, key=lambda x: x[-1], reverse=True)[:k]
+
+
+def seg_map(segmented_subject_dir, unsegmented_subject_dir, segmentation_dir, filenames, parameter_maps=None,
+            strict=False):
+    """Intra-subject segmentation mappings from supplied filenames
+
+    :param segmented_subject_dir: Directory with data of segmented image
+    :param unsegmented_subject_dir: Directory with data of unsegmented_image
+    :param segmentation_dir: Directory with data of segmented image segmentation
+    :param filenames: Iterable of filenames to map
+    :param parameter_maps: Optional vector of 3 parameter maps to be used for
+                           registration. If none are provided, a default vector
+                           of [rigid, affine, bspline] parameter maps is used.
+    :param strict: Default False. If True, a ValueError will be raised when some filename is not present in every
+                   supplied directory.
+
+    :rtype: [SimpleITK.Image]
+
+    >>> us_data = os.path.join(os.path.sep, 'srv', 'ultrasound_data')
+    >>> sub1 = os.path.join(us_data, 'sub1')
+    >>> sub2 = os.path.join(us_data, 'sub2')
+    >>> sub1_trials = os.path.join(sub1, 'trials')
+    >>> sub2_trials = os.path.join(sub2, 'trials')
+    >>> sub1_seg = os.path.join(sub1, seg)
+    >>> sub2_hand_shoulder_seg = seg_map(sub1_trials, sub2_trials, sub1_seg, ['trial18_90_fs_volume.mha'])
+    """
+    result_segs = []
+    for f in filenames:
+        unsegmented_image = os.path.join(unsegmented_subject_dir, f)
+        segmented_image = os.path.join(segmented_subject_dir, f)
+        segmentation = os.path.join(segmentation_dir, f)
+
+        if not all([os.path.isfile(image) for image in [unsegmented_image, segmented_image, segmentation]]):
+            if strict:
+                raise ValueError("File {} is not in all supplied directories".format(f))
+            continue
+
+        result_segs.append(segment(unsegmented_image, segmented_image, segmentation, parameter_maps=parameter_maps))
+
+    return result_segs
+
+
+def seg_map_all(segmented_subject_dir, unsegmented_subject_dir, segmentation_dir,
+                parameter_maps=None, image_type='volume', strict=False):
+    """Intra-subject segmentation mappings
+
+    Like seg_map, but selects all files of image_type in supplied directories as filename selection.
+
+    :param segmented_subject_dir: Directory with data of segmented image
+    :param unsegmented_subject_dir: Directory with data of unsegmented_image
+    :param segmentation_dir: Directory with data of segmented image segmentation
+    :param parameter_maps: Optional vector of 3 parameter maps to be used for
+                           registration. If none are provided, a default vector
+                           of [rigid, affine, bspline] parameter maps is used.
+    :param image_type: Either 'volume' or 'slice' corresponding to extensions '.mha' or '.nii', respectively
+    :param strict: Default False. If True, a ValueError will be raised when some filename is not present in every
+                   supplied directory.
+
+    :rtype: [SimpleITK.Image]
+
+    >>> us_data = os.path.join(os.path.sep, 'srv', 'ultrasound_data')
+    >>> sub1 = os.path.join(us_data, 'sub1')
+    >>> sub2 = os.path.join(us_data, 'sub2')
+    >>> sub1_trials = os.path.join(sub1, 'trials')
+    >>> sub2_trials = os.path.join(sub2, 'trials')
+    >>> sub1_seg = os.path.join(sub1, seg)
+    >>> sub2_segs = seg_map_all(sub1_trials, sub2_trials, sub1_seg)
+    """
+    sub1_images = _image_set(segmented_subject_dir, image_type=image_type)
+    sub2_images = _image_set(unsegmented_subject_dir, image_type=image_type)
+
+    matches = sub1_images.intersection(sub2_images)
+    return seg_map(segmented_subject_dir, unsegmented_subject_dir, segmentation_dir, matches,
+                   parameter_maps=parameter_maps, strict=strict)
 
 
 ##########################
 # Private module helpers #
 ##########################
 
+def _image_set(dirname, image_type='volume'):
+    if image_type == 'volume':
+        ext = '.mha'
+    elif image_type == 'slice':
+        ext = '.nii'
+    else:
+        raise ValueError("kwarg image_type must be either 'volume' or 'slice'")
+    images = glob.glob(os.path.join(dirname, '*{}'.format(ext)))
+    return set(os.path.basename(image) for image in images)
+
 
 def _to_elastix(pm, ttype):
     elastix_pm = sitk.GetDefaultParameterMap(ttype)
-    for k, v in pm.iteritems():
-        elastix_pm[k] = [v]
+    if sys.version_info[0] >=3:
+        it = pm.items()
+    else:
+        it = pm.iteritems()
+    for k, v in it:
+        if type(v) == list:
+            elastix_pm[k] = v
+        else:
+            elastix_pm[k] = [v]
     return elastix_pm
 
 
@@ -282,18 +452,30 @@ def _sim_score(candidate, ground_truth):
     return overlap_filter.GetDiceCoefficient()
 
 
+
+
 def _nn_assoc(pms):
     return _pm_vec_assoc('ResampleInterpolator',
                          'FinalNearestNeighborInterpolator', pms)
 
+def _nn_assoc_indv(pm):
+    return _pm_assoc('ResampleInterpolator',
+                         'FinalNearestNeighborInterpolator', pm)
 
 def _auto_init_assoc(pms):
     return _pm_vec_assoc('AutomaticTransformInitialization', 'true', pms)
 
+def _auto_init_assoc_indv(pm):
+    return _pm_assoc('AutomaticTransformInitialization', 'true', pm)
+
 
 def _pm_assoc(k, v, pm):
     result = {}
-    for key, val in pm.iteritems():
+    if sys.version_info[0] >=3:
+        it = pm.items()
+    else:
+        it = pm.iteritems()
+    for key, val in it:
         if key == k:
             result[key] = [v]
         else:
@@ -363,15 +545,15 @@ DEFAULT_AFFINE = {
     "DefaultPixelValue": ['0.000000'],
     "FinalBSplineInterpolationOrder": ['3.000000'],
     "FixedImagePyramid":
-        ['FixedSmoothingImagePyramid', 'FixedRecursiveImagePyramid'],
+        ['FixedSmoothingImagePyramid', 'FixedRecursiveImagePyramid'],#first one
     "ImageSampler": ['RandomCoordinate'],
-    "Interpolator": ['BSplineInterpolator'],
-    "MaximumNumberOfIterations": ['1024.000000'],
+    "Interpolator": ['BSplineInterpolator'],#Linear Interpolator
+    "MaximumNumberOfIterations": ['1024.000000'],#256
     "MaximumNumberOfSamplingAttempts": ['8.000000'],
     "Metric": ['AdvancedMattesMutualInformation'],
     "MovingImagePyramid": ['MovingSmoothingImagePyramid'],
     "NewSamplesEveryIteration": ['true'],
-    "NumberOfHistogramBins": ['32.000000'],
+    "NumberOfHistogramBins": ['32.000000'],#nonexistant
     "NumberOfResolutions": ['4.000000'],
     "NumberOfSamplesForExactGradient": ['4096.000000'],
     "NumberOfSpatialSamples": ['2048.000000'],
@@ -390,19 +572,19 @@ DEFAULT_BSPLINE = {
     'CheckNumberOfSamples': ["true"],
     'DefaultPixelValue': ['0.000000'],
     'FinalBSplineInterpolationOrder': ['3.000000'],
-    'FinalGridSpacingInPhysicalUnits': ['4.000000', '6.000000'],
+    'FinalGridSpacingInPhysicalUnits': ['4.000000', '6.000000'],#8
     'FixedImagePyramid': ['FixedSmoothingImagePyramid'],
-    'ImageSampler': ['RandomCoordinate'],
+    'ImageSampler': ['RandomCoordinate'],#Missing gridspacingschedule
     'Interpolator': ['LinearInterpolator'],
-    'MaximumNumberOfIterations': ['1024.000000'],
+    'MaximumNumberOfIterations': ['1024.000000'],#256
     'MaximumNumberOfSamplingAttempts': ['8.000000'],
     'Metric':
-        ['AdvancedMattesMutualInformation', 'TransformBendingEnergyPenalty'],
-    'Metric0Weight': ['0', '0.5', '1.000000', '2.0'],
+        [['AdvancedMattesMutualInformation', 'TransformBendingEnergyPenalty']],
+    'Metric0Weight': ['0', '0.5', '1.000000', '2.0'], #1.0
     'Metric1Weight': ['1.000000'],
     'MovingImagePyramid': ["MovingSmoothingImagePyramid"],
     'NewSamplesEveryIteration': ['true'],
-    'NumberOfHistogramBins': ['32.000000'],
+    'NumberOfHistogramBins': ['32.000000'],#nonexistant
     'NumberOfResolutions': ['4.000000'],
     'NumberOfSamplesForExactGradient': ['4096.000000'],
     'NumberOfSpatialSamples': ['2048.000000'],
